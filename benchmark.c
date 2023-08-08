b0ac9da9df9ce2344a946eb72d3a4397c00e3825
Qiantan Hong <qhong@alum.mit.edu>
2023-08-08 03:20:44 -0700

    add SIMD

--- benchmark.c
+++ benchmark.c
@@ -9,8 +9,28 @@ UBENCH_EX(cdr_coding_test, cdr_coding_nthcdr) {
   zsi_ref big_cons = zsi_iota_cdr_coding (10000, 0);
 
   UBENCH_DO_BENCHMARK() {
-    for (size_t i = 5000; i < 5001; i++) {
+    for (size_t i = 5000; i < 5100; i++) {
       temp = zsi_nthcdr (big_cons, i);
+    }
+  }
+}
+
+UBENCH_EX(cdr_coding_test, cdr_coding_nthcdr_simd) {
+  zsi_ref big_cons = zsi_iota_cdr_coding (10000, 0);
+
+  UBENCH_DO_BENCHMARK() {
+    for (size_t i = 5000; i < 5100; i++) {
+      temp = zsi_nthcdr_simd (big_cons, i);
+    }
+  }
+}
+
+UBENCH_EX(cdr_coding_test, cdr_coding_nthcdr_avx512) {
+  zsi_ref big_cons = zsi_iota_cdr_coding (10000, 0);
+
+  UBENCH_DO_BENCHMARK() {
+    for (size_t i = 5000; i < 5100; i++) {
+      temp = zsi_nthcdr_avx512 (big_cons, i);
     }
   }
 }
@@ -20,7 +40,7 @@ UBENCH_EX(cdr_coding_test, normal_compacted_nthcdr) {
   zsi_ref big_cons = zsi_iota_normal_compacted (10000, 0);
 
   UBENCH_DO_BENCHMARK() {
-    for (size_t i = 5000; i < 5001; i++) {
+    for (size_t i = 5000; i < 5100; i++) {
       temp = zsi_nthcdr (big_cons, i);
       /* zsi_debug_println (zsi_nthcdr (big_cons, i)); */
     }
@@ -31,7 +51,7 @@ UBENCH_EX(cdr_coding_test, normal_nthcdr) {
   zsi_ref big_cons = zsi_iota_normal (10000, 0);
 
   UBENCH_DO_BENCHMARK() {
-    for (size_t i = 5000; i < 5001; i++) {
+    for (size_t i = 5000; i < 5100; i++) {
       temp = zsi_nthcdr (big_cons, i);
       /* zsi_debug_println (zsi_nthcdr (big_cons, i)); */
     }
--- cdr-coding.c
+++ cdr-coding.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#ifndef __APPLE__
 #include <malloc.h>
+#endif
+#include <immintrin.h>
 
 typedef struct unused_struct *zsi_ref;
 typedef uintptr_t zsi_bits;
@@ -42,7 +45,7 @@ zsi_ref
 zsi_car (zsi_ref cons)
 {
   zsi_bits bits = ZSI_UNPACK (cons);
-  zsi_pair *p = (zsi_pair *) (bits & (~ZSI_TYPE_TAG_MASK));
+  zsi_pair *p = (zsi_pair *) (bits);
 
   bits = ZSI_UNPACK (p->car) & ~0x1;
   return ZSI_PACK (bits);
@@ -53,13 +56,63 @@ zsi_ref
 zsi_cdr (zsi_ref cons)
 {
   zsi_bits bits = ZSI_UNPACK (cons);
-  zsi_pair *p = (zsi_pair *) (bits & (~ZSI_TYPE_TAG_MASK));
+  zsi_pair *p = (zsi_pair *) (bits);
 
   if ((ZSI_UNPACK (p->car) & 0x1) & (ZSI_UNPACK (p->cdrs[0]) & 0x1)) {
     return ZSI_PACK ((zsi_bits) p->cdrs);
   } else {
     return p->cdrs[0];
   }
+}
+
+zsi_ref
+zsi_cddr (zsi_ref cons)
+{
+  zsi_bits bits = ZSI_UNPACK (cons);
+  zsi_pair *p = (zsi_pair *) (bits);
+  zsi_ref *cdrs = p->cdrs;
+
+  switch(_mm_tzcnt_32(_mm_testn_epi64_mask(_mm_loadu_si128(p),
+                                            _mm_set_epi64x(0x1,0x1)))){
+    case 0: return zsi_cdr(cdrs[0]);
+    case 1: return cdrs[1];
+    default: return ZSI_PACK(ZSI_UNPACK(p+2));}
+}
+
+zsi_ref
+zsi_cddddr (zsi_ref cons)
+{
+  zsi_bits bits = ZSI_UNPACK (cons);
+  zsi_pair *p = (zsi_pair *) (bits);
+  zsi_ref *cdrs = p->cdrs;
+
+  switch(_mm_tzcnt_32(_mm256_testn_epi64_mask(_mm256_loadu_si256(p),
+                                               _mm256_set_epi64x(0x1,0x1,0x1,0x1)))){
+    case 0: return zsi_cdr((zsi_cddr(cdrs[0])));
+    case 1: return zsi_cddr(cdrs[1]);
+    case 2: return zsi_cdr(cdrs[2]);
+    case 3: return cdrs[3];
+    default: return ZSI_PACK(ZSI_UNPACK(p+4));}
+}
+
+zsi_ref
+zsi_c8dr (zsi_ref cons)
+{
+  zsi_bits bits = ZSI_UNPACK (cons);
+  zsi_pair *p = (zsi_pair *) (bits);
+  zsi_ref *cdrs = p->cdrs;
+
+  switch(_mm_tzcnt_32(_mm512_testn_epi64_mask(_mm512_loadu_si512(p),
+                                               _mm512_set_epi64(0x1,0x1,0x1,0x1,0x1,0x1,0x1,0x1)))){
+    case 0: return zsi_cdr(zsi_cddr(zsi_cddddr(cdrs[0])));
+    case 1: return zsi_cddr(zsi_cddddr(cdrs[1]));
+    case 2: return zsi_cdr(zsi_cddddr(cdrs[2]));
+    case 3: return zsi_cddddr(cdrs[3]);
+    case 4: return zsi_cdr(zsi_cddr(cdrs[4]));
+    case 5: return zsi_cddr(cdrs[5]);
+    case 6: return zsi_cdr(cdrs[6]);
+    case 7: return cdrs[7];
+    default: return ZSI_PACK(ZSI_UNPACK(p+8));}
 }
 
 zsi_ref
@@ -84,6 +137,22 @@ zsi_ref
 zsi_nthcdr (zsi_ref list, size_t num)
 {
   for (size_t i = 0; i < num; list = zsi_cdr (list), i++);
+  return list;
+}
+
+zsi_ref
+zsi_nthcdr_simd (zsi_ref list, size_t num)
+{
+  for (size_t i = 0; i < num>>2; list = zsi_cddddr (list), i++);
+  for (size_t i = 0; i < (num&3); list = zsi_cdr (list), i++);
+  return list;
+}
+
+zsi_ref
+zsi_nthcdr_avx512 (zsi_ref list, size_t num)
+{
+  for (size_t i = 0; i < num>>3; list = zsi_c8dr (list), i++);
+  for (size_t i = 0; i < (num&7); list = zsi_cdr (list), i++);
   return list;
 }
 
